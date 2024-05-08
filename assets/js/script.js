c4888785303782d6da387d6a267e525ce4b6a02c
Kunal Kotkar <kunalkotkar28@gmail.com>
2024-05-08 01:17:44 -0400

    updated_page

--- assets/js/script.js
+++ assets/js/script.js
@@ -134,8 +134,6 @@ for (let i = 0; i < formInputs.length; i++) {
   });
 }
 
-
-
 // page navigation variables
 const navigationLinks = document.querySelectorAll("[data-nav-link]");
 const pages = document.querySelectorAll("[data-page]");
